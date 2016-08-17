02446ebce0cdd271d6bbce4bcba006fffbf697e6
Nathan Rohm <nathanrohm@gmail.com>
2016-08-17 04:58:31 +0000

    adds quote create

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,4 +7,18 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
 
+  def create
+    @quote = Quote.new(quote_params)
+    if @quote.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
 end
